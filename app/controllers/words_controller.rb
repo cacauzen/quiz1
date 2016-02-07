bc98dbde7fd35d0d9357114a1fffded60ec3b975
Claudia Almeida <ihavenids@gmail.com>
2016-02-07 23:18:44 +0000

    Done with Quiz 2

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,19 @@ class WordsController < ApplicationController
 	def index
 		@word = Word.order("RANDOM()").first
 	end
+
+	def new
+		@word = Word.new
+	end 
+
+	def create
+		Word.create(word_params)
+		redirect_to root_path
+	end	
+
+	private
+
+	def word_params
+		params.require(:word).permit(:advice, :author)
+	end	
 end
